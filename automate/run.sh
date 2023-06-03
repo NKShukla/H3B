029f6659a8688b8488c9bf9423a59bb9a0ab5365
Naval <naval19065@iiitd.ac.in>
2023-06-03 19:06:40 +0530

    Update run.sh

--- automate/run.sh
+++ automate/run.sh
@@ -5,9 +5,9 @@ sudo sysctl -w net.ipv4.ip_forward=1
 
 for i in {1..100}
 do
-	bash automate.sh dynamic-high.com- "video-groups/video-ids" $i
-	bash automate.sh dynamic-low.com- "video-groups/video-ids" $i
-	bash automate.sh 64-256-64-inc.com- "video-groups/video-ids" $i
-	bash automate.sh pakistan.com- "video-groups/video-ids" $i	
+	bash automate_chrome.sh dynamic-high.com- "video-groups/video-ids" $i
+	bash automate_chromium.sh dynamic-low.com- "video-groups/video-ids" $i
+	bash automate_firefox.sh 64-256-64-inc.com- "video-groups/video-ids" $i
+	bash automate_chrome.sh pakistan.com- "video-groups/video-ids" $i	
 done
 
